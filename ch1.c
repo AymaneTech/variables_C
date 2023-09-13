f3c71e77d02e4645a721dd63ae757ff00c8700e9
aymane <elmainiaymane03@gmail.com>
2023-09-13 13:18:27 +0200

    i'm done from challenge 1

---
+++ ch1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+    long long phone_num;
+    char name[30], last_name[30], sexe;
+
+    printf("Enter your name: ");
+    scanf("%s", name);
+    printf("Enter your last name: ");
+    scanf("%s", last_name);
+    printf("Enter your gender (M / F): ");
+    scanf(" %c", &sexe);
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    printf("Enter your phone number: ");
+    scanf("%lld", &phone_num);
+
+    printf("Name: %s\n", name);
+    printf("Last name: %s\n", last_name);
+    printf("Gender: %c\n", sexe);
+    printf("Age: %d\n", age);
+    printf("Phone number: %lld\n", phone_num);
+
+    return 0;
+}
