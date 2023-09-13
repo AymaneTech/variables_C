3651f94a941ae4ce3fa40a1e13f6f6d366219810
salma <soumasalma2030@gmail.com>
2023-09-13 13:45:04 +0100

    i'm done from  ch 1

---
+++ salma_app/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+ int age=19;
+ char prenom[40]= "salma";
+ char Nom[40] = "nbiga";
+ char sexe = "female";
+ int numro = 0770273364;
+ printf("%d,age");
+
+    return 0;
+}
