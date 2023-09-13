6a7bf899a4eb4a0b13f645d3a9768e71c1e9d57c
salma <soumasalma2030@gmail.com>
2023-09-13 13:48:14 +0100

    i' done from ch 2

--- salma_app/main.c
+++ salma_app/main.c
@@ -4,12 +4,8 @@
 int main()
 {
 
- int age=19;
- char prenom[40]= "salma";
- char Nom[40] = "nbiga";
- char sexe = "female";
- int numro = 0770273364;
- printf("%d,age");
+ float f,c;
+ printf("entre la temperater : ");
 
     return 0;
 }
