64657513d0395df5e2e01228a8b5f8e52b564d65
salma <soumasalma2030@gmail.com>
2023-09-13 15:35:15 +0100

    j' ai modifie le challenge 1

--- salma_app/main.c
+++ salma_app/main.c
@@ -3,9 +3,26 @@
 
 int main()
 {
+ int age;
+ char prenom[20];
+ char nom[20];
+ char sexe[20];
+ int numero;
 
- float f,c;
- printf("entre la temperater : ");
+  printf("entrez votre age :");
+  scanf("%d",&age);
+  printf("entrez votre pronom");
+  scanf("%s",&pronom);
+  printf("entrez votre nom");
+  scant("%s",&nom);
+  prantf("entrez votre sexe");
+  scanf("%s",&sexe);
+  printf("entrez votre numero");
+  scanf("%d,numero");
+  printf("%d\n%s\n%s\n%s\n%d\n,Age,pronom,nom,sexe,numero");
 
-    return 0;
+
+
+
+  return 0;
 }
